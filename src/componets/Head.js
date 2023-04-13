c57dbd328ae028e633084b25308ad0928d6a31bc
arunar1 <arunarofficial00@gmail.com>
2023-04-13 19:10:19 +0530

    commit

---
+++ src/componets/Head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Head.css'
+
+export default function Head() {
+  return (
+    <div className='heading'>
+        <h1>HOSPO</h1>
+    </div>
+  )
+}
---
+++ src/componets/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Login.css'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className='loginpage'>
+
+        
+      </div>
+    )
+  }
+}
+
+export default Login
