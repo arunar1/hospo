55b091e16b96b07a35a18af7ec93b7e31adb6f57
arunar1 <arunarofficial00@gmail.com>
2023-04-14 15:17:17 +0530

    HospoFront pageDesign

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './Login.css'
+import LoginRegistration from './logincomponent/LoginRegistration'
 
 class Login extends Component {
   render() {
     return (
       <div className='loginpage'>
-
-        
+        <LoginRegistration/>
       </div>
     )
   }
---
+++ src/componets/logincomponent/LoginRegistration.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import './LognR.css'
+
+
+class LoginRegistration extends Component {
+  render() {
+    return (
+      <div>
+        <div className='registration'>
+        <button>Login</button>
+        <button>Registration</button>
+        </div>
+       
+      </div>
+    )
+  }
+}
+
+export default LoginRegistration
