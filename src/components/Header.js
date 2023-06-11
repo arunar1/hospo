4a7cdf3a4dd2c4673f03ccc8edf9b72b72444508
arunar1 <arunarofficial00@gmail.com>
2023-06-12 01:59:09 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,26 @@
 import React from "react"; 
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import './Header.css';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = event => {
+    setIsActive(current => !current);
+  };
   return (
     <header>
         <div className="logo">
             <a>Hospo</a>
             <h1>HOSPO</h1>
         </div>
-        <div className="nav">
-        <ul>
+        
+        <div className='nav'>
+        <FontAwesomeIcon className="fontaw" icon={faBars} onClick={handleClick}>menu</FontAwesomeIcon>
+        <ul className={isActive ? 'menu' : ''}>
             <li><Link to="/" className='link'>Home</Link></li>
             <li><Link to='/about' className='link'>About</Link></li>
             <li><Link to='/contacts' className='link'>Contact</Link></li>
