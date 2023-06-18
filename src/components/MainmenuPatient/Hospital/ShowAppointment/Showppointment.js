99ddc78ee6e8a846472c4619a7f42dc792117595
arunar1 <arunarofficial00@gmail.com>
2023-06-18 15:01:55 +0530

    commit

--- src/components/MainmenuPatient/Hospital/ShowAppointment/Showppointment.js
+++ src/components/MainmenuPatient/Hospital/ShowAppointment/Showppointment.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom'
 import './ShowAppointment.css'
 import { useEffect,useState } from 'react'
 import axios from 'axios'
+import moment from 'moment'
 export default function ShowAppointment(props) {
-  const token=window.localStorage.getItem("token");
+  const myDate = new Date();
+  const formattedDate = moment(myDate).format('YYYY-MM-DD');
 
+  const token=window.localStorage.getItem("token");
+  const [dates,setdates]=useState('');
     const [appdetails,setappdetails]=useState([]);
+    const [view,setview]=useState()
   useEffect(()=>{
     axios.get(`${process.env.REACT_APP_URL}/appointmentinfo`,{
       headers:{token}
@@ -14,22 +19,31 @@ export default function ShowAppointment(props) {
     .then(res=>{
       setappdetails(res.data);
     })
-  },[])
+  },[view])
 
+const select=()=>{
+  setview(dates)
+}
+
+console.log(view)
   
 const [appdata,setappdata]=useState([])
 
   useEffect(()=>{
     const  newdata=[];
     appdetails.map((data)=>{
-        if(data.govhospitalname==props.details.hospitalname || data.govhospitalname==props.details.name){
+      
+        if((data.govhospitalname==props.details.hospitalname || data.govhospitalname==props.details.name) && data.date==view){
 
           newdata.push(data)
         }
         
     },setappdata(newdata)
     )
-  },[appdetails])
+  },[view])
+
+
+  
 
 
   return (
@@ -38,34 +52,43 @@ const [appdata,setappdata]=useState([])
         <div className='showappointmentHeader'>
             <h1>Show Appointments</h1>
             <ul>
-            {props.details.usertype=='hospital'?<li><Link to="/hospitalhome" className='linker'>back</Link></li>:<li><Link to="/consultanthome" className='linker'>back</Link></li>}
+            {props.details.usertype=='privateconsultant'?<li><Link to="/consultanthome" className='linker'>back</Link></li>:<li><Link to="/hospitalhome" className='linker'>back</Link></li>}
             </ul>
         </div>
+        
+        <div className='setdate'>
+        <h4>Select Date :</h4>
+          <input type='date'  name='select' onChange={(e)=>setdates(e.target.value)}/>
+          <button onClick={select}>select</button>
+        </div>
         <div>
-        <table>
-        <thead>
-          <tr>
-            <th>Sl no</th>
-            <th>Date of Appointment</th>
-            <th>Patient Name</th>
-            <th>Contact Number</th>
-            <th>Time</th>
-            <th>Token</th>
-          </tr>
-        </thead>
-        <tbody>
-          {appdata.map((appointment,index) => (
+        {appdata.length==0?(<h3>No appointments on {view}</h3>):(
+          <table>
+          <thead>
             <tr>
-              <td>{index+1}</td>
-              <td>{appointment.date}</td>
-              <td>{appointment.patientname}</td>
-              <td>{appointment.patientphone}</td>
-              <td>{appointment.time}</td>
-              <td>{appointment.token}</td>
+              <th>Sl no</th>
+              <th>Date of Appointment</th>
+              <th>Patient Name</th>
+              <th>Contact Number</th>
+              <th>Time</th>
+              <th>Token</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {appdata.map((appointment,index) => (
+              <tr>
+                <td>{index+1}</td>
+                <td>{appointment.date}</td>
+                <td>{appointment.patientname}</td>
+                <td>{appointment.patientphone}</td>
+                <td>{appointment.time}</td>
+                <td>{appointment.token}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+          
+        )}
         </div>
       </div>
     </div>
