a19daf8384c84c0fd3fe0190b158222eeefff93a
arunar1 <arunarofficial00@gmail.com>
2023-06-25 13:37:39 +0530

    commit

--- src/components/MainmenuPatient/Hospital/HospitalHeader.js
+++ src/components/MainmenuPatient/Hospital/HospitalHeader.js
@@ -25,7 +25,7 @@ const logout=()=>{
                 <li><Link to='/hospitalhome/showappointment' className='selctionbutton'>Show Appointment</Link></li>
                 <li><Link to='/hospitalhome/allappointment' className='selctionbutton'>Show All Appointment</Link></li>
                 <li><Link to='/hospitalhome/setappointment' className='selctionbutton'>Set Appointment Time</Link></li>
-                <li><Link to='/hospitalhome/editappointment' className='selctionbutton'>Edit Appointment</Link></li>
+                <li><Link to='/hospitalhome/editappointment' className='selctionbutton'>Delete Appointment</Link></li>
               </ul>
             </div>
         </div>
