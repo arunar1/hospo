aa36f973f68cbd306b33c862892f8fbeaa1186a2
arunar1 <arunarofficial00@gmail.com>
2023-06-27 00:29:39 +0530

    commit

--- src/components/Forgotpass/Forgotpass.js
+++ src/components/Forgotpass/Forgotpass.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import './Forgotpass.css'
 export default function Forgotpass() {
   return (
-    <div>
+    <div className='pass'>
+        <div className='passfor'>
+            <h3>forgot password is not enabled under development</h3>
+        </div>
       
     </div>
   )
