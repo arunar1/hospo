474a2c295940f552c6878a41bbc076d9041fde6e
arunar1 <arunarofficial00@gmail.com>
2023-07-02 05:19:00 +0530

    commit

--- src/components/MainmenuPatient/Hospital/ShowAppointment/Showppointment.js
+++ src/components/MainmenuPatient/Hospital/ShowAppointment/Showppointment.js
@@ -19,7 +19,7 @@ export default function ShowAppointment(props) {
     .then(res=>{
       setappdetails(res.data);
     })
-  },[view,props.details])
+  },[view,formattedDate])
 
 const select=()=>{
   setview(dates)
@@ -40,7 +40,7 @@ const [appdata,setappdata]=useState([])
         
     },setappdata(newdata)
     )
-  },[view,props.details,dates])
+  },[view,props.details])
 
 
   
